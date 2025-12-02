0ca62b3c33f6bed59861413337b3f31dd266b26d
hylandpad <hylandpad@gmail.com>
2025-12-02 14:12:36 -0500

    final part 1

--- main.py
+++ main.py
@@ -13,15 +13,25 @@ current = 50
 # Main loop
 for x in data:
     coeff,val = x[0],int(x[1:])
-    if coeff == 'R':
-        current += val
-        if current > 99:
-            current -= 100
-    else:
-        current -= val
-        if current < 0:
-            current += 100
-    print(current)
+
+    print(f'Current value is {current}. Moving dial {val} to the {coeff}')
+    
+    val = val%100
+    
+    if coeff == 'L':
+        val *= -1
+    
+    current += val
+    
+    if current < 0:
+        current = 100 + current
+    elif current > 100:
+        current = abs(100 - current)
+        
+    if current == 100:
+        current = 0
+
     if current == 0:
+        print(f'0 found!')
         counter += 1
 print(f'Password is {counter}')
